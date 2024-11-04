2a9a7871d3bd6154f88ec3782324c3aa0f905868
AndrianaKhondoka <andrianakhimich94@gmail.com>
2024-11-04 12:31:30 +0200

    add files to Lesson1.

---
+++ Lesson1/#6Qb97gsv/main.js
@@ -0,0 +1,5 @@
+let firstName = 'Андріана';
+let middleName = 'Віталіївна';
+let lastName = 'Хондока';
+let person =`${lastName} ${firstName} ${middleName}`;
+console.log(person);
---
+++ Lesson1/#ruUtWDUI/main.js
@@ -0,0 +1,6 @@
+let name = prompt('Ім`я', 'Андріана');
+console.log(name);
+let middleName = prompt('По-батькові', 'Віталіївна');
+console.log(middleName);
+let age = prompt('Вік', '30');
+console.log(age);
